89987becd41ee1f4e9f8a3080eab254dfdde8515
Daniel.Lin <daniel@daniellindemacbook-pro.local>
2019-06-01 13:21:38 +0800

    高级OpenGL/实例化 1. 通过新建一个VBO将offset传入顶点坐标layout中(这个可以为数据分散时使用)，然后通过glVertexAttribDivisor()指令绘制方式。

--- RTX_TEST/main.cpp
+++ RTX_TEST/main.cpp
@@ -316,10 +316,29 @@ int main()
     glBindBuffer(GL_ARRAY_BUFFER,planeVBO);
     glBufferData(GL_ARRAY_BUFFER,sizeof(planeVertices),planeVertices,GL_STATIC_DRAW);
     
-    glVertexAttribPointer(0,3,GL_FLOAT,GL_FALSE,5*sizeof(float),(void*)0);
     glEnableVertexAttribArray(0);
-    glVertexAttribPointer(1,2,GL_FLOAT,GL_FALSE,5*sizeof(float),(void*)(3*sizeof(float)));
+    glVertexAttribPointer(0,3,GL_FLOAT,GL_FALSE,5*sizeof(float),(void*)0);
     glEnableVertexAttribArray(1);
+    glVertexAttribPointer(1,2,GL_FLOAT,GL_FALSE,5*sizeof(float),(void*)(3*sizeof(float)));
+    
+    glm::vec2 translations[100];
+    int index=0;
+    float offset=0.1f;
+    for (int y= -10; y<10; y+=2) {
+        for (int x=-10; x<10; x+=2) {
+            glm::vec2 translation;
+            translation.x=(float)x/10.0f+offset;
+            translation.y=(float)y/10.0f+offset;
+            translations[index++]=translation;
+        }
+    }
+    unsigned int instanceVBO;
+    glGenBuffers(1,&instanceVBO);
+    glBindBuffer(GL_ARRAY_BUFFER,instanceVBO);
+    glEnableVertexAttribArray(2);
+    glBufferData(GL_ARRAY_BUFFER,sizeof(glm::vec2)*100,&translations[0],GL_STATIC_DRAW);
+    glVertexAttribPointer(2,2,GL_FLOAT,GL_FALSE,2*sizeof(float),(void*)0);
+    glVertexAttribDivisor(2,1);
     
     unsigned int skyboxVBO,skyboxVAO;
     glGenVertexArrays(1,&skyboxVAO);
@@ -487,17 +506,8 @@ int main()
     glActiveTexture(GL_TEXTURE2);
     glBindTexture(GL_TEXTURE_CUBE_MAP,textureID);
     
-    glm::vec2 translations[100];
-    int index=0;
-    float offset=0.1f;
-    for (int y= -10; y<10; y+=2) {
-        for (int x=-10; x<10; x+=2) {
-            glm::vec2 translation;
-            translation.x=(float)x/10.0f+offset;
-            translation.y=(float)y/10.0f+offset;
-            translations[index++]=translation;
-        }
-    }
+
+    
     // render loop
     // -----------
     while (!glfwWindowShouldClose(window))
