010f22a22a916737c1059a5a953b021e70b01162
Daniel.Lin <daniel@daniellindemacbook-pro.local>
2019-06-01 10:30:48 +0800

    高级OpenGL/几何着色器 1. 一个点画成五边形，再让其带上颜色。 2. 画成多边形则输出triangle_strip。带上颜色，则是在vertShader里传入颜色，在geoShader里面传入颜色并发射，在fragShader里面画出来就好。 3. 值得注意的是在geoShader中传入的颜色需要为数组，即vColor[].

--- RTX_TEST/main.cpp
+++ RTX_TEST/main.cpp
@@ -207,10 +207,10 @@ int main()
     };
     
     float points[]={
-        -0.5f,0.5f,
-        0.5f,0.5f,
-        0.5,-0.5,
-        -0.5,-0.5
+        -0.5f,0.5f,1,0,0,
+        0.5f,0.5f,0,1,0,
+        0.5,-0.5,0,0,1,
+        -0.5,-0.5,1,1,0
     };
     
     std::vector<Vertex> vertices;
@@ -352,8 +352,10 @@ int main()
     glBindBuffer(GL_ARRAY_BUFFER,geoVBO);
     glBufferData(GL_ARRAY_BUFFER,sizeof(points),points,GL_STATIC_DRAW);
     
-    glVertexAttribPointer(0,2,GL_FLOAT,GL_FALSE,2*sizeof(float),(void*)0);
+    glVertexAttribPointer(0,2,GL_FLOAT,GL_FALSE,5*sizeof(float),(void*)0);
     glEnableVertexAttribArray(0);
+    glVertexAttribPointer(1,3,GL_FLOAT,GL_FALSE,5*sizeof(float),(void*)(2*sizeof(float)));
+    glEnableVertexAttribArray(1);
     
     
     glBindBuffer(GL_ARRAY_BUFFER, 0);
